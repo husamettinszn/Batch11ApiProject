a0bfbe5e7bf3325f5485a332dc2e3a631f89b754
husamettinszn <husamettinszn@gmail.com>
2021-02-03 20:11:39 +0100

    w

---
+++ src/main/java/TestData/JsonPlaceHolderTestData.java
@@ -0,0 +1,15 @@
+package TestData;
+
+import java.util.HashMap;
+
+public class JsonPlaceHolderTestData {
+
+    HashMap<String, Object> expectedDataMap;
+
+    public  HashMap<String, Object> setUpData(){
+        expectedDataMap = new HashMap<>();
+        expectedDataMap.put()
+
+
+    }
+}
--- src/test/java/com/techproed/GetRequest11.java
+++ src/test/java/com/techproed/GetRequest11.java
@@ -1,8 +1,11 @@
 package com.techproed;
 
+import TestData.JsonPlaceHolderTestData;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 import testbase.TestBaseJsonPlaceHolder;
 
 import java.util.HashMap;
@@ -30,16 +33,9 @@ public class GetRequest11 extends TestBaseJsonPlaceHolder {
 
     @Test
     public void get01(){
-    spec01.pathParams("todos","todos", "id",2);
+    spec01.pathParams("todos","todos", "id",34);
     //Expected datayi olustur
-        HashMap<String,Object> expectedDataMap = new HashMap<>();
 
-        expectedDataMap.put("Status Code", 200);
-        expectedDataMap.put("userId", 1);
-        expectedDataMap.put("title", "quis ut nam facilis et officia qui");
-        expectedDataMap.put("completed", false);
-        expectedDataMap.put("Via", "1.1 vegur");
-        expectedDataMap.put("Server", "cloudflare");
 
 
         Response response = given().
@@ -48,16 +44,44 @@ public class GetRequest11 extends TestBaseJsonPlaceHolder {
                 get("/{todos}/{id}");
         response.prettyPrint();
 
+        JsonPlaceHolderTestData expectedObj = new JsonPlaceHolderTestData();
+
+        expectedObj.setUpData();
         //1.yol ile (body) assert edelim
 
         response.
                 then().
                 assertThat().
-                statusCode(200).
-                body("userId", Matchers.equalTo(1),
-                        "id",Matchers.equalTo(2),
-                        "title",Matchers.equalTo("quis ut nam facilis et officia qui"),
-                        "completed", Matchers.equalTo(false));
+                statusCode((Integer)expectedDataMap.get("Status Code")).
+                body("completed", Matchers.equalTo(expectedDataMap.get("completed")),
+                        "title", Matchers.equalTo(expectedDataMap.get("title")),
+                        "userId", Matchers.equalTo(expectedDataMap.get("userId")))
+                .headers("Via", expectedDataMap.get("Via"),"Server", expectedDataMap.get("Server"));
+
+        //2.Yol
+        HashMap<String, Object> actualDataMap = response.as(HashMap.class);
+        System.out.println(actualDataMap);
+
+        Assert.assertEquals(expectedDataMap.get("Status Code"), response.getStatusCode());
+
+        Assert.assertEquals(expectedDataMap.get("userId"), actualDataMap.get("userId"));
+
+        Assert.assertEquals(expectedDataMap.get("completed"), actualDataMap.get("title"));
+
+        Assert.assertEquals(expectedDataMap.get("Via"), response.getHeader("Via"));
+
+        Assert.assertEquals(expectedDataMap.get("Server"), response.getHeader("Server"));
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(response.getStatusCode(), expectedDataMap.get("Status Code"));
+        softAssert.assertEquals(actualDataMap.get("userId"), expectedDataMap.get("userId"));
+        softAssert.assertEquals(actualDataMap.get("title"), expectedDataMap.get("completed"));
+        softAssert.assertEquals(response.getHeader("Via"), expectedDataMap.get("Via"));
+        softAssert.assertEquals(response.header("Server"), expectedDataMap.get("Server"));
+
+        softAssert.assertAll();
+
 
 
 
