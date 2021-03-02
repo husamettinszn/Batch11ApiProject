596c77230f15e663bc3a4a3c47ba44816b58d3d2
husamettinszn <husamettinszn@gmail.com>
2021-03-02 15:18:25 +0100

    08

--- src/main/java/TestData/DummyTestData.java
+++ src/main/java/TestData/DummyTestData.java
@@ -1,5 +1,7 @@
 package TestData;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,15 +9,15 @@ import java.util.Map;
 
 public class DummyTestData {
 
-    public List<Map<String,Object>> expectedDataList= new ArrayList<>();
+    public List<Map<String, Object>> expectedDataList = new ArrayList<>();
 
-    public List<Map<String,Object>>  setUpData(){
-        HashMap<String, Object> expectedMap1= new HashMap<>();
-        HashMap<String, Object> expectedMap2= new HashMap<>();
-        HashMap<String, Object> expectedMap3= new HashMap<>();
-        HashMap<String ,Object> expectedMap4= new HashMap<>();
-        HashMap<String, Object> expectedMap5= new HashMap<>();
-        HashMap<String, Object> expectedMap6= new HashMap<>();
+    public List<Map<String, Object>> setUpData() {
+        HashMap<String, Object> expectedMap1 = new HashMap<>();
+        HashMap<String, Object> expectedMap2 = new HashMap<>();
+        HashMap<String, Object> expectedMap3 = new HashMap<>();
+        HashMap<String, Object> expectedMap4 = new HashMap<>();
+        HashMap<String, Object> expectedMap5 = new HashMap<>();
+        HashMap<String, Object> expectedMap6 = new HashMap<>();
  /*
       When
       I send get request to yje URL http://dummy.restapiexample.com/api/v1/employees
@@ -56,9 +58,9 @@ public class DummyTestData {
         expectedDataList.add(expectedMap5);
 
 
-        Map<String , String > empDetailsMap = new HashMap<>();
+        Map<String, String> empDetailsMap = new HashMap<>();
         empDetailsMap.put("id", "11");
-        empDetailsMap.put("employee_name","Jena Gaines");
+        empDetailsMap.put("employee_name", "Jena Gaines");
         empDetailsMap.put("employee_salary", "90560");
         empDetailsMap.put("employee_age", "30");
         empDetailsMap.put("profile_image", "");
@@ -68,7 +70,8 @@ public class DummyTestData {
 
         return expectedDataList;
     }
-    public Map<String, Integer> setUPData2(){
+
+    public Map<String, Integer> setUPData2() {
         Map<String, Integer> expectedDataMap = new HashMap<>();
         expectedDataMap.put("Status code", 200);
         expectedDataMap.put("highest_salary", 725000);
@@ -76,9 +79,10 @@ public class DummyTestData {
         expectedDataMap.put("second_highest_salary", 675000);
         expectedDataMap.put("highest_age", 66);
 
-       return expectedDataMap;
+        return expectedDataMap;
     }
-    public Map<String, String> setUpData3(){
+
+    public Map<String, String> setUpData3() {
 
         Map<String, String> reqBodyMap = new HashMap<>();
         reqBodyMap.put("name", "Ahmet Aksoy");
@@ -89,5 +93,30 @@ public class DummyTestData {
         return reqBodyMap;
     }
 
+    public Map<String, String> setUpMessageWithMap() {
+        Map<String, String> messageMap = new HashMap<>();
+        messageMap.put("status", "success");
+        messageMap.put("message", "Successfully! Record has been added.");
 
+        return messageMap;
+    }
+
+    public JSONObject setUpPostWithJSONOBJECT() {
+        JSONObject reqBodyJSONObject = new JSONObject();
+        reqBodyJSONObject.put("name", "Ahmet Aksoy");
+        reqBodyJSONObject.put("salary", "1000");
+        reqBodyJSONObject.put("age", "18");
+        reqBodyJSONObject.put("profil_image", "");
+
+        return reqBodyJSONObject;
+    }
+
+    public JSONObject setUpMessageWithJSONOBJECT() {
+        JSONObject messageJsonObject = new JSONObject();
+        messageJsonObject.put("status", "success");
+        messageJsonObject.put("message", "Successfully! Record has been added.");
+
+        return messageJsonObject;
+
+    }
 }
--- src/test/java/com/techproed/PostRequest01Tekrar.java
+++ src/test/java/com/techproed/PostRequest01Tekrar.java
@@ -2,10 +2,16 @@ package com.techproed;
 
 import TestData.DummyTestData;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 import testbase.TestBaseDummy;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -37,8 +43,8 @@ public class PostRequest01Tekrar extends TestBaseDummy {
     public void post011(){
         spec03.pathParam("createPath", "create");
 
-        DummyTestData expectedObj = new DummyTestData();
-        Map<String,String> reqBodyMap= expectedObj.setUpData3();
+        DummyTestData expectedObj= new DummyTestData();
+        Map<String, String> reqBodyMap= expectedObj.setUpData3();
         System.out.println(reqBodyMap);
 
         Response response = given().
@@ -49,9 +55,61 @@ public class PostRequest01Tekrar extends TestBaseDummy {
                 body(reqBodyMap).
                 when().
                 post("/{createPath}");
-
         response.prettyPrint();
-        System.out.println("YAdir");
+
+        //JSonPath
+        JsonPath jsonPath = response.jsonPath();
+
+        Assert.assertEquals(reqBodyMap.get("name"), jsonPath.getString("data.name"));
+        Assert.assertEquals(reqBodyMap.get("salary"), jsonPath.getString("data.salary"));
+        Assert.assertEquals(reqBodyMap.get("age"), jsonPath.getString("data.age"));
+
+        if(reqBodyMap.get("profile_image").equals("")){
+            reqBodyMap.put("profile_image", null);
+        }
+        Assert.assertEquals(reqBodyMap.get("profile_image"), jsonPath.get("data.profile_image"));
+
+        Map<String, String> expectedMsgMap= expectedObj.setUpMessageWithMap();
+        System.out.println(expectedMsgMap);
+
+        Assert.assertEquals(expectedMsgMap.get("success"), jsonPath.getString("data.success"));
+        Assert.assertEquals(expectedMsgMap.get("message"), jsonPath.getString("message"));
+
+        //De_Serialization GSON
+        Map<String, Object> actualDataMap = response.as(HashMap.class);
+        System.out.println(actualDataMap);
+
+        Assert.assertEquals(reqBodyMap.get("name"), ((Map)actualDataMap.get("data")).get("name"));
+        Assert.assertEquals(reqBodyMap.get("age"), ((Map) actualDataMap.get("data")).get("age"));
+        Assert.assertEquals(reqBodyMap.get("salary"), ((Map) actualDataMap.get("data")).get("salary"));
+
+        /*
+         if(reqBodyMap.get("profile_image").equals("")){
+            reqBodyMap.put("profile_image", null);
+        }
+         Yukarida yaptigimiz icin gerek yok.
+         */
+        Assert.assertEquals(reqBodyMap.get("profile_image"), ((Map) actualDataMap.get("data")).get("profile_image"));
+        Assert.assertEquals(expectedMsgMap.get("status"), actualDataMap.get("status"));
+        Assert.assertEquals(expectedMsgMap.get("message"), actualDataMap.get("message"));
+
+        //JSONOBJECT ile ve JsonPath ile
+        SoftAssert softAssert = new SoftAssert();
+
+        JSONObject expectedDataJsonObject = expectedObj.setUpPostWithJSONOBJECT();
+        System.out.println(expectedDataJsonObject);
+
+        softAssert.assertEquals(jsonPath.getString("data.name"), expectedDataJsonObject.getString("name"));
+        softAssert.assertEquals(jsonPath.getString("data.salary"), expectedDataJsonObject.getString("salary"));
+        softAssert.assertEquals(jsonPath.getString("data.age"), expectedDataJsonObject.getString("age"));
+        //softAssert.assertEquals(jsonPath.getString("data.profil_image"), expectedDataJsonObject.getString("profil_image"));
+
+        JSONObject msgJsonObject = expectedObj.setUpMessageWithJSONOBJECT();
+        System.out.println(msgJsonObject);
+
+        softAssert.assertEquals(jsonPath.getString("status"), msgJsonObject.getString("status"));
+
+        softAssert.assertAll();
 
 
     }
